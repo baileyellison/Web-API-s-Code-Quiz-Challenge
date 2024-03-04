a150e7ef753fd748e5f0c98ca7fba5c7af7a0284
Bailey Ellison <beeusc2018@gmail.com>
2024-03-04 18:10:55 -0500

    Added Elements to JS

---
+++ Assets/js/script.js
@@ -0,0 +1,14 @@
+// Get all Elements
+const startBtn = document.querySelector (".startBtn button");
+const quizBox = document.querySelector (".quizBox");
+const Exit = quizBox.querySelector (".buttons .Exit");
+const Restart = quizBox.querySelector (".buttons .Restart");
+
+// Starting Quiz If Statements
+startBtn.oneclick = () {
+    quizBox.classList.add("");
+}
+
+
+
+
