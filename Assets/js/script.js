03ed6d09432607a94f64e3e2c9a9bc042ec3b811
Bailey Ellison <beeusc2018@gmail.com>
2024-03-04 20:29:54 -0500

    Added a Questions JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,17 +1,24 @@
 // Get all Elements
 const startBtn = document.querySelector (".startBtn button");
-const infoCard = document.querySelector (".quizBox");
-const Exit = infoCard.querySelector (".buttons .Exit");
-const Restart = infoCard.querySelector (".buttons .Restart");
+const infoCard = document.querySelector (".infoCard");
+const Exit_btn = infoCard.querySelector (".buttons .Exit");
+const Continue = infoCard.querySelector (".buttons .Continue");
+const quizBox = document.querySelector (".quizBox");
 
 // Starting Button
-startBtn.oneclick = ()=>{
+startBtn.onclick = ()=>{
     infoCard.classList.add("activeInfo");
 }
 
-// Exit Butotn
-Exit.oneclick = ()=>{
+// Exit Button
+Exit_btn.onclick = ()=>{
+     infoCard.classList.remove("activeInfo");
+}
+
+// Show Quiz Box
+Continue.onclick = ()=>{
     infoCard.classList.remove("activeInfo");
+    quizBox.classList.add("activeQuiz");
 }
 
 
